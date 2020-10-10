8e38664fd173a4d0b50d01d0b6051b7ec3cce674
anhducc13 <trantienduc10@gmail.com>
2020-10-10 07:06:42 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Router, Switch, Route } from 'react-router-dom';
 import { Spin } from 'antd';
-import TekoID from 'teko-oauth2';
-import ReactTracker from 'react-tracker-teko';
-import { commonConstants } from 'constants/index';
 import { browserHistory } from 'helpers';
 import { t } from 'helpers/i18n';
 import './App.less';
@@ -11,19 +8,9 @@ import './App.scss';
 import Page403 from 'containers/shared/Page403';
 import Page404 from 'containers/shared/Page404';
 import Page500 from 'containers/shared/Page500';
+import Login from 'containers/Login';
 import PrivateRoute from 'components/shared/PrivateRoute';
 import AppLayout from 'containers/AppLayout';
-
-const { iam, tracker } = window.config;
-
-const { IAM_SCOPES } = commonConstants;
-
-const reactTracker = new ReactTracker({
-  // Configure your tracker server and site by providing
-  host: (tracker.appId && tracker.host) || '',
-  urlServeJsFile: (tracker.appId && tracker.jsFile) || '',
-  appId: tracker.appId || '',
-});
 
 const App: React.FC = () => {
   const [loading] = useState(false);
@@ -31,8 +18,9 @@ const App: React.FC = () => {
   return loading ? (
     <Spin className="app-spin" />
   ) : (
-    <Router history={reactTracker.connectToHistory(browserHistory)}>
+    <Router history={browserHistory}>
       <Switch>
+        <Route exact path="/login" name="Login" component={Login} />
         <Route exact path="/403" name="403" component={Page403} />
         <Route exact path="/404" name="404" component={Page404} />
         <Route exact path="/500" name="500" component={Page500} />
--- src/containers/Login/Login.tsx
+++ src/containers/Login/Login.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Form } from 'antd';
+import { Form, Input, Button, Row } from 'antd';
 import styled from '@emotion/styled';
+
+const FormItem = Form.Item;
 
 const FormWrapper = styled.div`
   position: absolute;
@@ -26,5 +28,51 @@ const FormWrapper = styled.div`
   }
 `;
 
-const StyledLogo = styled.div``
-const Login = () => {};
+const StyledLogo = styled.div`
+  text-align: center;
+  cursor: pointer;
+  margin-bottom: 24px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    width: 25px;
+    margin-right: 8px;
+  }
+
+  span {
+    vertical-align: text-bottom;
+    font-size: 16px;
+    text-transform: uppercase;
+    display: inline-block;
+    font-weight: 700;
+    color: #eb1f3a;
+  }
+`;
+
+const Login = () => {
+  return (
+    <FormWrapper>
+      <StyledLogo>
+        <img alt="Logo" src="./favicon.png" />
+        <span>Ductt Site</span>
+      </StyledLogo>
+      <Form onFinish={() => {}}>
+        <FormItem name="username" rules={[{ required: true }]} hasFeedback>
+          <Input placeholder="Username" />
+        </FormItem>
+        <FormItem name="password" rules={[{ required: true }]} hasFeedback>
+          <Input type="password" placeholder="Password" />
+        </FormItem>
+        <Row>
+          <Button type="primary" htmlType="submit">
+            Đăng nhập
+          </Button>
+        </Row>
+      </Form>
+    </FormWrapper>
+  );
+};
+
+export default Login;
